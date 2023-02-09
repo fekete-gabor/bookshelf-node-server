a86afcfcab1722d16d84059d5bc4251419a7f64d
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-02-09 17:44:31 +0100

    getAllBooks now uses query params

--- controllers/bookshelfControllers.js
+++ controllers/bookshelfControllers.js
@@ -1,17 +1,64 @@
 const Book = require("../models/BookModel");
 
 const getAllBooks = async (req, res) => {
-  const books = await Book.find({ createdBy: req.user.userID });
+  const { author, title } = req.query;
 
-  if (books.length === 0) {
+  const allBooks = await Book.find({ createdBy: req.user.userID });
+
+  let books;
+  let queryObj = {};
+
+  if (allBooks.length === 0) {
     return res.status(200).json({
       success: true,
-      numberOfBooks: books.length,
+      numberOfBooks: allBooks.length,
       msg: "Looks like there is nothing here, try to add some books...",
     });
   }
 
-  res.status(200).json({ success: true, numberOfBooks: books.length, books });
+  if (author.length === 0 && title.length === 0) {
+    books = allBooks;
+    return res.status(200).json({
+      success: true,
+      numberOfBooks: books.length,
+      books,
+    });
+  }
+
+  if (author.length <= 2 && title.length === 0) {
+    return res
+      .status(500)
+      .json({ msg: "name must be at least 3 letters long!" });
+  }
+
+  if (author.length > 0 && title.length === 0) {
+    queryObj.authors = { $regex: author, $options: "i" };
+  }
+
+  if (author.length === 0 && title.length > 0) {
+    queryObj.title = { $regex: title, $options: "i" };
+  }
+
+  if (author.length > 0 && title.length > 0) {
+    queryObj.authors = { $regex: author, $options: "i" };
+    queryObj.title = { $regex: title, $options: "i" };
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.maxResults) || 10;
+  const skip = (page - 1) * limit;
+
+  books = await Book.find(queryObj).skip(skip).limit(limit);
+
+  const totalBooks = await Book.countDocuments(queryObj);
+  const numberOfPages = Math.ceil(totalBooks / limit);
+
+  return res.status(200).json({
+    success: true,
+    numberOfBooks: books.length,
+    numberOfPages,
+    books,
+  });
 };
 
 const getSingleBook = async (req, res) => {
