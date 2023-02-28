3fa009c0e006174b8945c4a5400028a04dd3eb20
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-02-28 16:00:23 +0100

    created edit model

---
+++ models/EditModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const EditSchema = new mongoose.Schema(
+  {
+    id: String,
+    bookEdits: [
+      {
+        category: String,
+        inputs: [{ id: Number, name: String, desc: String }],
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "please provide a user"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Edit", EditSchema);
