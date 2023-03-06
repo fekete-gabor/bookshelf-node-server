e9a2f1d718bc6b1ae90d44c26c98ccd8fe9a2425
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-03-06 15:39:42 +0100

    fixed update & delete note functions,  input id is created with crypto now

--- controllers/editControllers.js
+++ controllers/editControllers.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 const Edit = require("../models/EditModel");
+const crypto = require("crypto");
 
 const getAllCategories = async (req, res) => {
   const { userID } = req.user;
@@ -25,12 +25,6 @@ const getAllNotes = async (req, res) => {
     return res.status(404).send(`No book with id of ${bookID}`);
   }
 
-  if (!userID) {
-    return res
-      .status(401)
-      .send("User not found, please provide valid credentials!");
-  }
-
   let book = await Edit.findOne({ createdBy: userID, id: bookID });
 
   const findCategory = book.bookEdits.find(
@@ -46,20 +40,20 @@ const getAllNotes = async (req, res) => {
   res.status(200).json({ success: true, inputs });
 };
 
-const createCategoryBtn = async (req, res) => {
+const createCategory = async (req, res) => {
   const { userID } = req.user;
   const { id: bookID } = req.params;
-  const { name: fieldName } = req.body;
+  let { name: fieldName } = req.body;
 
   if (!bookID) {
     return res.status(404).send(`No book with id of ${bookID}`);
   }
 
-  if (!userID) {
-    return res
-      .status(401)
-      .send("User not found, please provide valid credentials!");
+  if (fieldName.length <= 2) {
+    return res.status(500).send("button name must be at least 3 letters long!");
   }
+
+  fieldName = fieldName.toLowerCase().trim();
 
   const book = await Edit.findOne({ createdBy: userID, id: bookID });
 
@@ -83,7 +77,7 @@ const createCategoryBtn = async (req, res) => {
   let findCategory = book.bookEdits.find((book) => book.category === fieldName);
 
   if (findCategory) {
-    return res.status(200).send("this category already exists!");
+    return res.status(500).send("this category already exists!");
   }
 
   book.bookEdits = [
@@ -99,19 +93,35 @@ const createCategoryBtn = async (req, res) => {
   return res.status(200).json({ success: true, book });
 };
 
-const createEdits = async (req, res) => {
+const deleteCategory = async (req, res) => {
+  const { userID } = req.user;
+  const { id: bookID } = req.params;
+  const { category } = req.query;
+
+  if (!bookID) {
+    return res.status(404).send(`No book with id of ${bookID}`);
+  }
+
+  let book = await Edit.findOne({ createdBy: userID, id: bookID });
+
+  book.bookEdits = book.bookEdits.filter((book) => book.category !== category);
+
+  await book.save();
+
+  const categories = [...new Set(book.bookEdits.map((book) => book.category))];
+
+  res
+    .status(200)
+    .json({ success: true, msg: "field successfully deleted!", categories });
+};
+
+const createNote = async (req, res) => {
   const { userID } = req.user;
   const { id: bookID } = req.params;
   const { categoryName, inputName, richText } = req.body;
 
   if (!bookID) {
     return res.status(404).send(`No book with id of ${bookID}`);
-  }
-
-  if (!userID) {
-    return res
-      .status(401)
-      .send("User not found, please provide valid credentials!");
   }
 
   let book = await Edit.findOne({ createdBy: userID, id: bookID });
@@ -121,13 +131,10 @@ const createEdits = async (req, res) => {
   );
 
   findCategory.inputs.push({
+    id: crypto.randomUUID(),
     name: inputName.name,
     desc: richText.desc,
   });
-
-  for (let i = 0; i < findCategory.inputs.length; i++) {
-    findCategory.inputs[i].id = i;
-  }
 
   await book.save();
 
@@ -136,36 +143,23 @@ const createEdits = async (req, res) => {
     .json({ success: true, book, msg: "Notes successfully created!" });
 };
 
-const updateEdits = async (req, res) => {
+const updateNote = async (req, res) => {
   const { userID } = req.user;
   const { id: bookID } = req.params;
-  const { categoryName, inputName, richText, editID } = req.body;
+  const { inputName, richText, editID } = req.body;
 
   if (!bookID) {
     return res.status(404).send(`No book with id of ${bookID}`);
   }
 
-  if (!userID) {
-    return res
-      .status(401)
-      .send("User not found, please provide valid credentials!");
-  }
-
   const book = await Edit.findOne({ createdBy: userID, id: bookID });
 
-  let findCategory = book.bookEdits.find(
-    (book) => book.category === categoryName
-  );
+  let input = book.bookEdits
+    .flatMap((book) => book.inputs)
+    .find((input) => input.id === editID);
 
-  findCategory = findCategory.inputs.filter((input) => {
-    if (input.id === editID) {
-      input.name = inputName.name;
-      input.desc = richText.desc;
-      return input;
-    } else {
-      return input;
-    }
-  });
+  input.name = inputName.name;
+  input.desc = richText.desc;
 
   await book.save();
 
@@ -174,15 +168,51 @@ const updateEdits = async (req, res) => {
     .json({ success: true, book, msg: "Field successfully updated!" });
 };
 
-const deleteEdits = async (req, res) => {
-  res.status(200).send("delete edits route");
+const deleteNote = async (req, res) => {
+  const { userID } = req.user;
+  const { id: bookID } = req.params;
+  const { id: btnID, category } = req.query;
+
+  if (!bookID) {
+    return res.status(404).send(`No book with id of ${bookID}`);
+  }
+
+  let book = await Edit.findOne({ createdBy: userID, id: bookID });
+
+  const newList = book.bookEdits.map((book) => {
+    if (book.category === category) {
+      const obj = {
+        ...book,
+        inputs: book.inputs.filter((input) => input.id !== btnID),
+      };
+
+      return obj;
+    } else {
+      return book;
+    }
+  });
+
+  book.bookEdits = newList;
+
+  await book.save();
+
+  const findCategory = book.bookEdits.find(
+    (book) => book.category === category
+  );
+
+  const { inputs } = findCategory;
+
+  res
+    .status(200)
+    .json({ success: true, msg: "notes successfully deleted!", inputs });
 };
 
 module.exports = {
   getAllCategories,
   getAllNotes,
-  createCategoryBtn,
-  createEdits,
-  updateEdits,
-  deleteEdits,
+  createCategory,
+  deleteCategory,
+  createNote,
+  updateNote,
+  deleteNote,
 };
