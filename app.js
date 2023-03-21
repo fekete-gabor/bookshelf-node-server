afaa4cf6d2203ef5eee5ae9bf2b1a570f94d83d6
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-03-21 16:06:07 +0100

    test

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cors());
 app.use(xss());
 
 // routes
-app.get("/", (req, res) => {
+app.get("/api/v1/test", (req, res) => {
   res.status(200).send("you are here!");
 });
 app.use("/api/v1/auth", user);
