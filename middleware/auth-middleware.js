de028a8252ef3996d339aa5a92d3f1aee779fa94
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-03-27 15:20:22 +0200

    asd

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -36,7 +36,7 @@ const auth = async (req, res, next) => {
 
     // create accessToken cookie
     res.cookie("accessToken", accessTokenJWT, {
-      httpOnly: true,
+      httpOnly: false,
       expires: new Date(Date.now() + oneHour),
       secure: process.env.NODE_ENV === "production" ? true : false,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
