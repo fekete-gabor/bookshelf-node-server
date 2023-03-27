f62e9c645ef138d501e949a789f4e590d3ddc792
fekete-gabor <91623519+fekete-gabor@users.noreply.github.com>
2023-03-27 15:41:35 +0200

    Revert "removed algorithm"
    This reverts commit 79b3ced65ff32c53059ae8ff3a510e624d3f08f1.

--- models/UserModel.js
+++ models/UserModel.js
@@ -57,7 +57,8 @@ UserSchema.pre("save", async function () {
 UserSchema.methods.createJWT = function (refreshToken) {
   const token = jwt.sign(
     { userID: this._id, name: this.name, email: this.email, refreshToken },
-    process.env.JWT_SECRET
+    process.env.JWT_SECRET,
+    { algorithm: HS256 }
   );
   return token;
 };
